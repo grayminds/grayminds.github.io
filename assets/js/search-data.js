776125b35379f227f01e1d5510b2c27baadb5f57
grayminds <35176664+grayminds@users.noreply.github.com>
2026-02-23 00:42:56 +0000

    Deploying to gh-pages from @ grayminds/grayminds.github.io@e1cbf8e47dd06c7ead8867c35203283faf55d9dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,49 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-ai-doesn-39-t-have-motivation-you-do",
+        },{id: "dropdown-tech-stack",
+              title: "Tech Stack",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/tech-stack/";
+              },
+            },{id: "dropdown-timeline-cat-tested",
+              title: "Timeline (Cat Tested)",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/timeline-cats/";
+              },
+            },{id: "dropdown-publications",
+              title: "Publications",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/publications/";
+              },
+            },{id: "dropdown-labyrinthian",
+              title: "Labyrinthian",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/labyrinthian/";
+              },
+            },{id: "dropdown-gray-minds-llc",
+              title: "Gray Minds LLC",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/graymindsllc/";
+              },
+            },{id: "dropdown-sky-time-studio",
+              title: "Sky Time Studio",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/skytimestudio/";
+              },
+            },{id: "post-ai-doesn-39-t-have-motivation-you-do",
         
           title: "AI Doesn&#39;t Have Motivation.  You Do.",
         
